a13d2b884ecff6ea564035f57da2500dbeedb76b
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2022-03-31 01:08:22 +0300

    rollback packages version to fix issues

--- src/board/board-sections/section/task-create/create-task.hook.ts
+++ src/board/board-sections/section/task-create/create-task.hook.ts
@@ -1,10 +1,28 @@
 import { useMutation } from '@apollo/client';
+import { TASK_FIELDS } from '../../../../common/fragments/task-fields.gql';
 import { CREATE_TASK } from './create-task.gql';
 import { CreateTask, CreateTaskVariables } from './__generated__/CreateTask';
 
 export const useCreateTask = () => {
   const [createTask, { loading }] = useMutation<CreateTask, CreateTaskVariables>(CREATE_TASK, {
-    refetchQueries: ['FetchSections'],
+    update(cache, { data }) {
+      const task = data?.createTask?.task;
+
+      if (task)
+        cache.modify({
+          id: cache.identify({ id: task?.sectionId, __typename: 'Section' }),
+          fields: {
+            tasks(existingTasks = []) {
+              const newTaskRef = cache.writeFragment({
+                fragment: TASK_FIELDS,
+                data: task,
+              });
+
+              return [...existingTasks, newTaskRef];
+            },
+          },
+        });
+    },
   });
 
   return { createTask, loading };
